8aeed17eba3666e07d29f04d387b1ce56822b5fe
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-11-16 18:42:11 -0500

    edit navbar for logo hover
    Edit code for navbar logo hover

--- _includes/navbar.php
+++ _includes/navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
     <div class="container">
-	    <span id="logo" href="<? echo $_SESSION['fileDirectory']; ?>index.php" class="navbar-brand"></span>
+	    <a href="<? echo $_SESSION['fileDirectory']; ?>index.php">
+	    	<span id="logo" class="navbar-brand"></span>
+	    </a>
 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 	    	<span class="navbar-toggler-icon"></span>
 		</button>		
