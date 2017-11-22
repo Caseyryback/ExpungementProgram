049efbd3d97b496706affb41e3f12c7d9febf82d
Caseyryback <32656244+Caseyryback@users.noreply.github.com>
2017-11-21 19:34:23 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <? require_once($_SESSION['fileDirectory']."_includes/navbar.php"); ?>
 <div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
 	<div id="contentContainer" class="container pt-1"><!-- contains all content -->
-            <img class="img-fluid mx-auto d-block" src="_images/placeholder.jpg" height="480" width="960" alt="placeholder">
+            <img class="img-fluid mx-auto d-block" src="_images/homePage2.jpg" height="480" width="960" alt="placeholder">
         <div class="container">            
             <div class="row py-2">
                 <span class="mx-auto d-block px-3 article" id="subTitle">Indiana's Leading Record Expungement Service </span>
@@ -30,19 +30,17 @@
                     committed to helping expunge all eligible arrests, misdemeanors, and felonies.
                 </h5>
                 <span class="mx-auto d-block px-3 article" id="subTitle">What is an Expungement &amp; Why Does Anyone Need One?</span>
-                <ul class="list-group py-3" style="list-style-type:none">
-                <li class="list-group-item list-group-item-success"><i class="fa fa-circle" aria-hidden="true">Expungement is the removal of criminal records from public inspection that come up in a Criminal Background Check.</i>
-                    <i class="fa fa-circle" aria-hidden="true">Currently in our society even after you have paid your debt to society you may still punished if you have a criminalrecord because of the prevalence of the internet and social media.</i>
-                    <i class="fa fa-circle" aria-hidden="true">Having a criminal record limits opportunities in: </i>
-                    <ul style="list-style-type:none">
-                        <i class="fa fa-arrow-circle-right" aria-hidden="true">Employment</i>
-                        <i class="fa fa-arrow-circle-right" aria-hidden="true">Loans</i>
-                        <i class="fa fa-arrow-circle-right" aria-hidden="true">Education</i>
-                        <i class="fa fa-arrow-circle-right" aria-hidden="true">Housing</i>
-                        <i class="fa fa-arrow-circle-right" aria-hidden="true">Public Benefits</i>
-                    </ul>
-                    <i class="fa fa-circle" aria-hidden="true">Your criminal record is never forgotten and never forgiven.</i>
-                </ul>
+                <h5 class="text-light py-3">
+                    <div><i id="circle" class="fa fa-circle py-2" aria-hidden="true"></i>  Expungement is the removal of criminal records from public inspection that come up in a Criminal Background Check.</div>
+                    <div><i id="circle" class="fa fa-circle py-2" aria-hidden="true"></i>  Currently in our society even after you have paid your debt to society you may still punished if you have a criminal record because of the prevalence of the internet and social media.</i>
+                    <div><i id="circle" class="fa fa-circle py-2" aria-hidden="true"></i>  Having a criminal record limits opportunities in: </i>
+                    <a><i id="check" class="fa fa-check" aria-hidden="true"></i>  Employment</a>
+                    <a><i id="check" class="fa fa-check" aria-hidden="true"></i>   Loans</a>
+                    <a><i id="check" class="fa fa-check" aria-hidden="true"></i>   Education</a>
+                    <a><i id="check" class="fa fa-check" aria-hidden="true"></i>   Housing</a>
+                    <a><i id="check" class="fa fa-check" aria-hidden="true"></i>   Public Benefits</a>
+                    <div><i id="circle" class="fa fa-circle py-2" aria-hidden="true"></i>  Your criminal record is never forgotten and never forgiven.</div>
+                </h5>
             </div> <!-- row 2 -->
         </div> <!-- row container -->
 	</div><!-- content container -->
