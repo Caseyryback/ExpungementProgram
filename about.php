d035cd6d235ac24febbd0af9aaff9f34ce51d9cc
Caseyryback <32656244+Caseyryback@users.noreply.github.com>
2017-12-04 16:38:27 -0500

    php changes
    regular web page changes for the week

--- about.php
+++ about.php
@@ -9,11 +9,34 @@
 <? require_once($_SESSION['fileDirectory']."_includes/navbar.php"); ?>
 <div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
 	<div id="contentContainer" class="container pt-1"><!-- contains all content -->
-        <div class="row">
-            <div class="col-sm-12 col-md-6">This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. </div>
-            <div class="col-sm-12 col-md-6">This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. </div>
-        </div>
-
+           <img class="img-fluid mx-auto d-block" src="_images/aboutOffice.jpg" height="480" width="960" alt="placeholder">
+        <div class="container">
+            <div class="row py-2">
+                <span class="mx-auto d-block px-3 article" id="subTitle">About </span>
+                <h5 class="text-light py-3">   
+                   The Northern Indiana Restorative Justice and Reentry Clinic is the only organization in Indiana specializing in pro bono and 
+                   low cost record expungement. 
+                </h5>
+                <span class="mx-auto d-block px-3 article" id="subTitle">Established in 2016</span>
+                <h5 class="text-light py-3"> 
+                    The Second Chance Program originally started in 2013 by Ivy Tech Professors Irene Britt and Marcus Ellison, with Ivy Tech 
+                    students helping their classmates and community members with the expungement process. The program began with the directive 
+                    of empowering students to obtain certifications, degrees, and jobs in fields traditionally closed off to those with criminal histories. 
+                    In response to demand from the larger South Bend community, the clinic expanded its services to all Indiana residents and became a registered 501(c)(3) 
+                    non-profit in 2016. 
+                </h5>
+                <span class="mx-auto d-block px-3 article" id="subTitle">Our Goal</span>
+                <h5 class="text-light py-3"> 
+                    We are driven by the belief that "Every Life Matters," and view the expungement process as an important equalizer within the 
+                    restorative justice process. At its core this program is about incorporating justice into the fabric and pulse of society, while 
+                    also ensuring that the community is not just a safe place to live, work, and play, but also a great place to be. 
+                </h5>
+                <h5 class="text-light py-3">
+​                    We hope to expand our operation over time to provide the range of services needed by those reentering the community, and we are always 
+                    looking for partners, collaborators, and donors to help us fulfill that vision. A community working together will make a difference. 
+                </h5>
+            </div>
+        </div><!-- container -->
 	</div><!-- content container -->
 </div><!-- main page container -->
 
