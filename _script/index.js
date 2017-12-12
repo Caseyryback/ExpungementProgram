a3a04e506c87b1d6b1cfc0477edae296fdb5a14d
Caseyryback <32656244+Caseyryback@users.noreply.github.com>
2017-12-11 20:34:53 -0500

    index script
    index script

--- _script/index.js
+++ _script/index.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+function circle() {
+	let circle = document.getElementsByClassName('bullet')
+	for (i = 0; i < circle.length; i++) {
+		circle[i].innerHTML = "&#xf111;";
+	}	
+}
+function check() {
+	let check = document.getElementsByClassName('check')
+	for (i = 0; i < check.length; i++) {
+		check[i].innerHTML = "&#xf00c;";
+	}
+}
+circle();
+check();
