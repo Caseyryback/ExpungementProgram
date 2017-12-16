e8c0a0d863542510263c021e515edaae13324d52
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-12-15 19:53:26 -0500

    Add files via upload

--- mission.js
+++ mission.js
@@ -1,20 +1,20 @@
-
-
-
-     function imgRandom() {
-         
-       var myImages = new Array();
-        myImages[5] = "_images/people1.jpg";
-        myImages[1] = "_images/people2.jpg";
-        myImages[2] = "_images/people3.jpg";
-        myImages[3] = "_images/people4.jpg";
-        myImages[4] = "_images/people5.jpg";
-         
-         
-            var rnd = Math.floor(Math.random() * people1.length);
-    if (rnd == 0) {
-            rnd = 1;
-    }
-    document.write(<img class="img-rounded" src="_images/people[i]">);
-}
+function imgRandom() {
+   
+   var target = document.getElementById("target");
      
+   var myImages = new Array();
+    myImages[5] = "_images/people1.jpg";
+    myImages[1] = "_images/people2.jpg";
+    myImages[2] = "_images/people3.jpg";
+    myImages[3] = "_images/people4.jpg";
+    myImages[4] = "_images/people5.jpg";
+     
+     
+    var rnd = Math.floor(Math.random() * myImages.length);
+    if (rnd == 0) {
+        rnd = 1;
+    }
+    console.log(rnd);
+    target.src=(myImages[rnd]);
+}
+    document.getElementById("submit").addEventListener("click", imgRandom);
